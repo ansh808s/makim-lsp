00f5510e9390fb32b566fdf9dff2d9981f15e0ed
bitbard3 <ansharora3839@gmail.com>
2025-05-30 14:17:11 +0530

    chore: setup ESlint

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import pluginJs from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+
+  {
+    languageOptions: { ecmaVersion: 2020, globals: globals.browser },
+  },
+  {
+    ignores: [
+      'coverage',
+      '**/public',
+      '**/dist',
+      'pnpm-lock.yaml',
+      'pnpm-workspace.yaml',
+    ],
+  },
+
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+];
